9511083294cd83338459ca67995c10c43d129624
ulysses-sl <mail@saklee.net>
2014-03-23 13:21:01 -0400

    140323-1

---
+++ 1-12.scm
@@ -0,0 +1,7 @@
+(define (pascal row col)
+  (cond ((or (< row 1) (< col 1) (> col row))
+         (error "wrong arguments"))
+        ((or (= col 1) (= col row))
+         1)
+        (else
+         (+ (pascal (- row 1) (- col 1)) (pascal (- row 1) col)))))
