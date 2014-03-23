72848805ac767f844239f2f1aab9aad803c198d3
ulysses-sl <mail@saklee.net>
2014-03-22 23:55:21 -0400

    140322-1

--- 1-11.scm
+++ 1-11.scm
@@ -1,7 +1,7 @@
 (define (f-rec n)
   (if (< n 3)
     n
-    (+ (f (- n 1)) (* 2 (f (- n 2))) (* 3 (f (- n 3))))))
+    (+ (f-rec (- n 1)) (* 2 (f-rec (- n 2))) (* 3 (f-rec (- n 3))))))
 
 (define (f-it n)
   (define (f-iter w x y z)
